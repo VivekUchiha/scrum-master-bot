fc1766cd31f8c6b1a8c4d3a02175df889507a544
shambu2k <sidharthshambu00@gmail.com>
2020-09-05 18:54:48 +0530

    fix: fixed stop scrum crash

--- commands/stop_scrum.js
+++ commands/stop_scrum.js
@@ -5,9 +5,16 @@ module.exports = {
 
             console.log(message);
 
-            client.isScrumHappening = false;
-
-            client.collector.stop();
+            if(client.isScrumHappening){
+                try {
+                    client.isScrumHappening = false;
+                    client.collector.stop();
+                } catch (error) {
+                    console.log(error);
+                }
+            } else {
+                message.channel.send("Start the scrum first");
+            }
             
         } else {
             message.channel.send("You are not the Real Scrum Master");
