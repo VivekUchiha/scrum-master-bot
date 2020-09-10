44704eacfa1192c38f83e0374c2460bcae897ccb
tejus <rameshtejus@gmail.com>
2020-09-10 17:59:54 +0530

    feat: Add cron job to get CTF events in the upcoming week

--- commands/remindScrum.js
+++ commands/remindScrum.js
@@ -4,10 +4,10 @@ module.exports={
         var content = ":exclamation: :exclamation: Reminder: Scrum at";
         args.forEach(arg=>{
             content += ` ${arg}`
-        })
+        });
         content += "\n If you're not gonna be present for the scrum, Send your reason here or else Vivek kik you out"  
         Array.from(message.guild.members.cache.values()).forEach(mem => {
             mem.user.send(content)
-        })
+        });
     }
 }
