c9beb9897947ecb7a539b31f4a0d885616936737
tejus <rameshtejus@gmail.com>
2020-09-10 10:46:46 +0530

    feat: add remin_scrum command

---
+++ commands/remindScrum.js
@@ -0,0 +1,13 @@
+module.exports={
+    name:"remind_scrum",
+    execute(client, message, args){
+        var content = ":exclamation: :exclamation: Reminder: Scrum at";
+        args.forEach(arg=>{
+            content += ` ${arg}`
+        })
+        content += "\n If you're not gonna be present for the scrum, Send your reason here or else Vivek kik you out"  
+        Array.from(message.guild.members.cache.values()).forEach(mem => {
+            mem.user.send(content)
+        })
+    }
+}
