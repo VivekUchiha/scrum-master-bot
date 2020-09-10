05a4e1566058494bb16ce14602144ad388fb94a4
tejus <rameshtejus@gmail.com>
2020-09-11 00:53:53 +0530

    fix(remind scrum): Send reminder to only current members

--- commands/remindScrum.js
+++ commands/remindScrum.js
@@ -1,13 +1,26 @@
-module.exports={
-    name:"remind_scrum",
-    execute(client, message, args){
-        var content = ":exclamation: :exclamation: Reminder: Scrum at";
-        args.forEach(arg=>{
-            content += ` ${arg}`
-        });
-        content += "\n If you're not gonna be present for the scrum, Send your reason here or else Vivek kik you out"  
-        Array.from(message.guild.members.cache.values()).forEach(mem => {
-            mem.user.send(content)
-        });
+module.exports = {
+    name: "remind_scrum",
+    execute(client, message, args) {
+        try {
+            var content = ":exclamation: :exclamation: Reminder: Scrum at";
+            args.forEach(arg => {
+                content += ` ${arg}`
+            });
+            content += "\n If you're not gonna be present for the scrum, Send your reason here or else Vivek kik you out";
+
+            if (!message.member.roles.cache.some(role => role.name === 'Real Scrum Master')) {
+                message.channel.send("Inna thala. You're not the Scrum Master");
+                return;
+            }
+            const CurrentRole = message.guild.roles.cache.find(role => role.name === 'Current');
+
+            Array.from(message.guild.members.cache.values()).forEach(mem => {
+                if (mem._roles.includes(CurrentRole.id))
+                    mem.user.send(content);
+            });
+        }
+        catch{
+            message.channel.send("Something went wrong");
+        }
     }
 }
