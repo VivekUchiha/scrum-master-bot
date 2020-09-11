44524c0c6d9c7eed43481d1f4d65e00f38ae65cf
Vivek Raveendran <satsvivek@gmail.com>
2020-09-11 17:35:40 +0530

    change crontab

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,5 +1,5 @@
 module.exports = (client, member) => {
 	console.log(member.displayName);
 	client.members.set(member.id, member);
-	member.send(`Welcome to delta server, ${member.displayName}. Enter \n !setrole alumnus (if you're an alumnus) \n !setrole current (if you're a current member)`);
+	member.send(`Welcome to delta server, ${member.displayName}. Enter \n !setrole alumnus (if you're an alumnus) or \n !setrole current (if you're a current member)`);
 };
